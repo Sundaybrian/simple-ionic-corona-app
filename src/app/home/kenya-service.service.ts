7a3d0d09df0edebe573e5b1eda865291d37f344f
Brian Sunday <brians931@gmail.com>
2020-03-25 16:38:15 +0300

    add flag for the country kenya

--- src/app/home/kenya-service.service.ts
+++ src/app/home/kenya-service.service.ts
@@ -5,38 +5,53 @@ import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 @Injectable({
   providedIn: "root"
 })
-
-
-
 export class KenyaService {
+  baseurl = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats";
 
-  baseurl = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
-  
-  
   httpOptions = {
-    headers : {
-      'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com",
-      'x-rapidapi-key': "dabceb15d5msh12757e7eeeaf772p1d80bdjsn15872dd7ce60"
+    headers: {
+      "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+      "x-rapidapi-key": "dabceb15d5msh12757e7eeeaf772p1d80bdjsn15872dd7ce60"
     },
     params: new HttpParams().set("country", "Kenya")
   };
-  
-  constructor(
-    private httpClient: HttpClient
-  ) {}
 
-  loadData(){
+  countries_url = 'assets/countries.json';
+
+  constructor(private httpClient: HttpClient) {}
+
+  loadData() {
+    // load data for kenya only
     return this.httpClient.get(`${this.baseurl}`, this.httpOptions);
   }
 
-  loadWorldData(){
-    return this.httpClient.get(`${this.baseurl}`,{
-      headers : new HttpHeaders({
-        'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com",
-        'x-rapidapi-key': "dabceb15d5msh12757e7eeeaf772p1d80bdjsn15872dd7ce60"
-      }),
-    
+  loadWorldData() {
+    // load all countries with their specific data
+    return this.httpClient.get(`${this.baseurl}`, {
+      headers: new HttpHeaders({
+        "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+        "x-rapidapi-key": "dabceb15d5msh12757e7eeeaf772p1d80bdjsn15872dd7ce60"
+      })
     });
   }
+
+  loadWorldTotalStat() {
+    // fetch aggregate world stats of cases
+    return this.httpClient.get(
+      "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php",
+      {
+        headers: new HttpHeaders({
+          "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+          "x-rapidapi-key": "dabceb15d5msh12757e7eeeaf772p1d80bdjsn15872dd7ce60"
+        })
+      }
+    );
+  }
+
+  getJson(){
+  return this.httpClient.get(this.countries_url);
+
+  }
+
 
 }
--- src/app/home/kenya/kenya.page.ts
+++ src/app/home/kenya/kenya.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { KenyaService } from "../kenya-service.service";
-import { Case } from "../models";
 
 @Component({
   selector: "app-kenya",
@@ -10,14 +9,39 @@ import { Case } from "../models";
 export class KenyaPage implements OnInit {
   isLoading = true;
   results: any;
+  worldstats: any;
   constructor(private KenyaService: KenyaService) {}
 
   ngOnInit() {
+    // fetch kenyastats
     this.KenyaService.loadData()
       .toPromise()
       .then((results: any) => {
         this.results = results;
         this.isLoading = false;
+        this.mapCountryToFlag(this.results.data.covid19Stats[0]);
+      });
+
+      // fetch world stats
+      this.KenyaService.loadWorldTotalStat().subscribe((result)=> {
+        this.worldstats = result;
+        console.log(this.worldstats);
+      });
+
+
+  }
+
+  mapCountryToFlag(obj) {
+    return this.KenyaService.getJson().toPromise().then(data => {
+         data['countries'].forEach(country => {
+          if(country["name"].toLowerCase() == obj['country'].toLowerCase()){
+             obj['flag'] = country['flag'];
+             this.results.data.covid19Stats[0] = obj;
+          }
+
+        });
+
       });
   }
+
 }
